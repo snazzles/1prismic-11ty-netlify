c04e073d09d0cd83362e57094e7d2e13f03af2b0
Martin K <martinkz@users.noreply.github.com>
2020-03-05 17:02:25 +0000

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('img');
-
   return {
-    dir: { 
-      input: 'src', 
-      output: '_site', 
+    dir: {
+      input: "src",
+      output: "_site",
       data: "_data"
-    },
-  }
-}
+    }
+  };
+};
